77db37e1c75cbc73c55c95fe38e7e84249515e46
ADITYA PRAKASH <98552380+adiprak@users.noreply.github.com>
2022-01-29 15:18:16 +0530

    Add files via upload

---
+++ MyFrame.java
@@ -0,0 +1,68 @@
+import javax.swing.*;
+import java.awt.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class MyFrame extends JFrame {
+
+    SimpleDateFormat timeFormat,dayFormat,dateFormat;
+    JLabel timeLabel;
+    JLabel dayLabel;
+    JLabel dateLabel;
+    String date;
+    String day;
+    String time;
+
+    MyFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("INDIAN CLOCK");
+        this.setLayout(new FlowLayout());
+        this.setSize(350 ,200);
+        this.setResizable(false);
+
+        timeFormat=new SimpleDateFormat("hh:mm:ss a");
+        dayFormat=new SimpleDateFormat("E");
+        dateFormat=new SimpleDateFormat("D");
+
+        timeLabel=new JLabel();
+        timeLabel.setFont(new Font("Verdana",Font.PLAIN,50));
+        timeLabel.setForeground(new Color(0x00FF00));
+        timeLabel.setBackground(Color.black);
+        timeLabel.setOpaque(true);
+
+        dayLabel=new JLabel();
+        dayLabel.setFont(new Font("Ink Free",Font.PLAIN,35));
+
+        dateLabel=new JLabel();
+        dateLabel.setFont(new Font("Ink Free",Font.PLAIN,35));
+
+        time=timeFormat.format(Calendar.getInstance().getTime());
+        timeLabel.setText(time);
+
+        this.add(timeLabel);
+        this.add(dayLabel);
+        this.add(dateLabel);
+        this.setVisible(true);
+
+        setTime();
+    }
+    public void setTime(){
+        while(true){
+            time=timeFormat.format(Calendar.getInstance().getTime());
+            timeLabel.setText(time);
+
+            day=dayFormat.format(Calendar.getInstance().getTime());
+            dayLabel.setText(day);
+
+            date=dateFormat.format(Calendar.getInstance().getTime());
+            dateLabel.setText(date);
+
+            try{
+            Thread.sleep(1000);
+            }
+            catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
