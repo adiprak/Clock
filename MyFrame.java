4a653bf52f99add77819f6e0ec98fcbaadae2a4d
ADITYA PRAKASH <98552380+adiprak@users.noreply.github.com>
2022-01-29 15:21:04 +0530

    Update MyFrame.java

--- MyFrame.java
+++ MyFrame.java
@@ -15,7 +15,7 @@ public class MyFrame extends JFrame {
 
     MyFrame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setTitle("INDIAN CLOCK");
+        this.setTitle("MY CLOCK");
         this.setLayout(new FlowLayout());
         this.setSize(350 ,200);
         this.setResizable(false);
